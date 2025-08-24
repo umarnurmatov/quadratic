78dac396da8533c098a27a41aa1ec068547c43f0
umarnurmatov <umarkhonnur12@gmail.com>
2025-08-24 20:55:22 +0300

    #FIXED added __attribute__((unused)) to potentially unused variables

--- include/colorutils.h
+++ include/colorutils.h
@@ -5,17 +5,19 @@
 
 #include "assertutils.h"
 
-static const char* ANSI_COLOR_RED    = "\x1b[1;31m";
-static const char* ANSI_COLOR_GREEN  = "\x1b[1;32m";
-static const char* ANSI_COLOR_YELLOW = "\x1b[1;33m";
-static const char* ANSI_COLOR_BLUE   = "\x1b[1;34m";
+#define UNUSED __attribute__((unused))
 
-static const char* ANSI_COLOR_BOLD_WHITE  = "\x1b[1;38;5;15m";
-static const char* ANSI_COLOR_BOLD_RED    = "\x1b[1;38;5;160m";
-static const char* ANSI_COLOR_BOLD_GREEN  = "\x1b[1;38;5;82m";
-static const char* ANSI_COLOR_BOLD_YELLOW = "\x1b[1;38;5;226m";
-static const char* ANSI_COLOR_BOLD_BLUE   = "\x1b[1;38;5;20m";
+UNUSED static const char* ANSI_COLOR_RED    = "\x1b[1;31m";
+UNUSED static const char* ANSI_COLOR_GREEN  = "\x1b[1;32m";
+UNUSED static const char* ANSI_COLOR_YELLOW = "\x1b[1;33m";
+UNUSED static const char* ANSI_COLOR_BLUE   = "\x1b[1;34m";
 
-static const char* ANSI_COLOR_RESET       = "\x1b[0m";
+UNUSED static const char* ANSI_COLOR_BOLD_WHITE  = "\x1b[1;38;5;15m";
+UNUSED static const char* ANSI_COLOR_BOLD_RED    = "\x1b[1;38;5;160m";
+UNUSED static const char* ANSI_COLOR_BOLD_GREEN  = "\x1b[1;38;5;82m";
+UNUSED static const char* ANSI_COLOR_BOLD_YELLOW = "\x1b[1;38;5;226m";
+UNUSED static const char* ANSI_COLOR_BOLD_BLUE   = "\x1b[1;38;5;20m";
+
+UNUSED static const char* ANSI_COLOR_RESET       = "\x1b[0m";
 
 void utils_colored_fprintf(FILE* stream, const char* mode, const char* fmtstring, ...);
