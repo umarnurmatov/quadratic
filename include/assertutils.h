6148f16976442eaf2933fa3c63ca527f395136ba
umarnurmatov <umarkhonnur12@gmail.com>
2025-08-25 14:18:48 +0300

    #ADDED backtrace print for assert

--- include/assertutils.h
+++ include/assertutils.h
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <execinfo.h>
 
 #include "colorutils.h"
 
@@ -9,6 +10,11 @@
                                     ? (void)(0) \
                                     : utils_assert_fail(#expression, __FILE__, __LINE__)
 
-/// @brief processes assertion failure case; see utils_assert macro
-/// @param expression string containing asserted expression; passed by macro
-void utils_assert_fail(const char* expression, const char* filename, int linenumber);
+/// @brief prints stack trace to stderr 
+void utils_print_stacktrace(void);
+
+/// @brief prints failed assertion info  and stack trace to stderr (see utils_assert macro)
+/// @param expression string containing asserted expression
+/// @param filename filename where assertion occured
+/// @param linenumber number of line containing assertion in file
+void __attribute__((noreturn)) utils_assert_fail(const char* expression, const char* filename, int linenumber);
--- src/assertutils.c
+++ src/assertutils.c
@@ -1,8 +1,28 @@
 #include "assertutils.h"
 
-void utils_assert_fail(const char* expression, const char* filename, int linenumber)
+static const int MAX_STACKTRACE_SIZE = 256;
+
+void utils_print_stacktrace(void)
+{
+    void** backtrace_buffer = (void**)calloc(MAX_STACKTRACE_SIZE, sizeof(void*));
+
+    int backtrace_size = backtrace(backtrace_buffer, MAX_STACKTRACE_SIZE);
+    char** backtracce_strings = backtrace_symbols(backtrace_buffer, backtrace_size);
+    if (backtracce_strings != NULL)
+    {
+        utils_colored_fprintf(stderr, ANSI_COLOR_BOLD_WHITE, "Stack trace (%d stack frames):\n", backtrace_size);
+        for(int i = 0; i < backtrace_size; ++i)
+            utils_colored_fprintf(stderr, ANSI_COLOR_YELLOW, "%s\n", backtracce_strings[i]);
+    }
+
+    free(backtracce_strings);
+    free(backtrace_buffer);
+}
+
+void utils_assert_fail(const char *expression, const char *filename, int linenumber)
 {
     utils_colored_fprintf(stderr, ANSI_COLOR_BOLD_WHITE, "%s:%d: assertion failed\n", filename, linenumber);
     utils_colored_fprintf(stderr, ANSI_COLOR_BOLD_RED, "   assert(%s)\n", expression);
+    utils_print_stacktrace();
     abort();
 }
