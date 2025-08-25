b445101e1c9e30ad192718445d5944563a95de40
umarnurmatov <umarkhonnur12@gmail.com>
2025-08-25 16:41:05 +0300

    #FIXED made tests be read from file

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ BUILD_DIR := build
 SRC_DIR := src
 TEST_DIR := test
 INCLUDE_DIR := include
-EXECUTABLE := quadratic
-TEST_EXECUTABLE := test_quadratic
+EXECUTABLE := quadratic.out
+TEST_EXECUTABLE := test_quadratic.out
 
 CC := g++
 CPPFLAGS := -MMD -MP -I$(INCLUDE_DIR) -D _DEBUG -ggdb3 -std=c++17 -O0 -Wall -Wextra -Weffc++ -Waggressive-loop-optimizations -Wc++14-compat -Wmissing-declarations -Wcast-align -Wcast-qual -Wchar-subscripts -Wconditionally-supported -Wconversion -Wctor-dtor-privacy -Wempty-body -Wfloat-equal -Wformat-nonliteral -Wformat-security -Wformat-signedness -Wformat=2 -Winline -Wlogical-op -Wnon-virtual-dtor -Wopenmp-simd -Woverloaded-virtual -Wpacked -Wpointer-arith -Winit-self -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=2 -Wsuggest-attribute=noreturn -Wsuggest-final-methods -Wsuggest-final-types -Wsuggest-override -Wswitch-default -Wswitch-enum -Wsync-nand -Wundef -Wunreachable-code -Wunused -Wuseless-cast -Wvariadic-macros -Wno-literal-suffix -Wno-missing-field-initializers -Wno-narrowing -Wno-old-style-cast -Wno-varargs -Wstack-protector -fcheck-new -fsized-deallocation -fstack-protector -fstrict-overflow -flto-odr-type-merging -fno-omit-frame-pointer -Wlarger-than=8192 -Wstack-usage=8192 -pie -fPIE -Werror=vla -fsanitize=address,alignment,bool,bounds,enum,float-cast-overflow,float-divide-by-zero,integer-divide-by-zero,leak,nonnull-attribute,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,undefined,unreachable,vla-bound,vptr
@@ -13,31 +13,31 @@ OBJS := $(patsubst %.c,$(BUILD_DIR)/%.o,$(notdir $(SOURCES)))
 DEPS := $(patsubst %.o,%.d,$(OBJS))
 
 # PROGRAM
-$(BUILD_DIR)/$(EXECUTABLE).out: $(OBJS)
-	$(CC) $(CPPFLAGS) $(OBJS) -o $@ $(LDFLAGS)
+$(BUILD_DIR)/$(EXECUTABLE): $(OBJS)
+	@$(CC) $(CPPFLAGS) $(OBJS) -o $@ $(LDFLAGS)
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.c 
 	@mkdir -p $(BUILD_DIR)
 	@$(CC) $(CPPFLAGS) -c $< -o $@
 
 .PHONY: run
-run: $(BUILD_DIR)/$(EXECUTABLE).out
-	./$(BUILD_DIR)/$(EXECUTABLE).out
+run: $(BUILD_DIR)/$(EXECUTABLE)
+	@./$(BUILD_DIR)/$(EXECUTABLE)
 
 # TEST
 TEST_SOURCES := $(filter-out %main.c,$(SOURCES)) $(wildcard $(TEST_DIR)/*.c)
 TEST_OBJS := $(patsubst %.c,$(BUILD_DIR)/%.o,$(notdir $(TEST_SOURCES)))
 
-$(BUILD_DIR)/$(TEST_EXECUTABLE).out: $(TEST_OBJS)
-	$(CC) $(CPPFLAGS) $(TEST_OBJS) -o $@ $(LDFLAGS)
+$(BUILD_DIR)/$(TEST_EXECUTABLE): $(TEST_OBJS)
+	@$(CC) $(CPPFLAGS) $(TEST_OBJS) -o $@ $(LDFLAGS)
 
 $(BUILD_DIR)/%.o: $(TEST_DIR)/%.c
 	@mkdir -p $(BUILD_DIR)
 	@$(CC) $(CPPFLAGS) -c $< -o $@
 
 .PHONY: test
-test: $(BUILD_DIR)/$(TEST_EXECUTABLE).out
-	./$(BUILD_DIR)/$(TEST_EXECUTABLE).out
+test: $(BUILD_DIR)/$(TEST_EXECUTABLE)
+	@./$(BUILD_DIR)/$(TEST_EXECUTABLE)
 
 .PHONY: clean
 clean:
--- test/test.c
+++ test/test.c
@@ -1,20 +1,163 @@
 #include "quadratic.h"
 #include "ioutils.h"
 
+static const size_t TEST_DATA_NUM_FIELDS = 5;
+/*
+Every single test is written in one line.
+Single test is structured as followed:
+1. No roots:  <c1> <c2> <c3> nan  nan
+2. 1 root:    <c1> <c2> <c3> <r1> nan
+3. 2 roots:   <c1> <c2> <c3> <r1> <r2>
+4. inf roots: <c1> <c2> <c3> inf  <any>
+5. linear:    <c1> <c2> <c3> nan  nan (yeah, we don't solve linear eq)
+*/
+static const char* TEST_INPUT_RELATIVE_FILEPATH = "test/input.txt";
+
+struct test_data_t
+{
+    double coeff_a;
+    double coeff_b;
+    double coeff_c;
+    double root_a;
+    double root_b;
+};
+
+enum test_err_t
+{
+    TEST_ERR_TEST_SUCCESS,
+    TEST_ERR_TEST_FAIL,
+    TEST_ERR_FILE_OPEN_ERR,
+    TEST_ERR_FILE_READ_ERR,
+    TEST_ERR_FILE_READ_SUCCESS
+};
+
+enum test_err_t all_test(const char* test_input_filename);
+enum test_err_t one_test(struct test_data_t* test_data);
+enum test_err_t read_one_test(char* filebuffer, struct test_data_t* test_data, int* bytes_read);
+int utils_equal_with_precision_nan_inf(double a, double b);
+
 int main()
 {
-    double coeff_a = 0.0, coeff_b = 0.0, coeff_c = 0.0;
-    double root_a = 0.0, root_b = 0.0;
-    enum root_cnt_t root_cnt;
-    for(int i = 0; i < 10; ++i) {
-        coeff_a = rand();
-        coeff_b = rand();
-        coeff_c = rand();
+    enum test_err_t test_err = all_test(TEST_INPUT_RELATIVE_FILEPATH);
 
-        utils_colored_fprintf(stdout, ANSI_COLOR_BOLD_BLUE, "Testing with coefficients: %lf %lf %lf\n", coeff_a, coeff_b, coeff_c);
-        root_cnt = solve_quadratic_equation(coeff_a, coeff_b, coeff_c, &root_a, &root_b);
-        print_quadratic_equation_solution(root_cnt, root_a, root_b);
-        puts("##########\n");
+    switch(test_err) {
+        case TEST_ERR_TEST_FAIL:
+        case TEST_ERR_FILE_OPEN_ERR:
+        case TEST_ERR_FILE_READ_ERR:
+            return 1;
+            break;
+        case TEST_ERR_TEST_SUCCESS:
+        case TEST_ERR_FILE_READ_SUCCESS:
+            break;
+        default:
+            break;
     }
+
     return 0;
 }
+
+enum test_err_t all_test(const char* test_input_filename)
+{
+    utils_assert(test_input_filename != NULL);
+
+    char* test_file_realpath = realpath(test_input_filename, NULL);
+    FILE* test_file = open_file(test_file_realpath, "r");
+    if(test_file == NULL)
+        return TEST_ERR_FILE_OPEN_ERR;
+    free(test_file_realpath);
+
+    char* test_file_buf = bufferize_file(test_file);
+    size_t test_file_size = get_file_size(test_file);
+    
+    char* test_file_buf_ptr = test_file_buf;
+    int bytes_read = 0;
+    int test_cnt = 0;
+    struct test_data_t test_data;
+    while(test_file_buf_ptr - test_file_buf < (long)test_file_size) {
+        if(read_one_test(test_file_buf_ptr, &test_data, &bytes_read) == TEST_ERR_FILE_READ_ERR)
+            return TEST_ERR_FILE_READ_ERR; 
+        if(one_test(&test_data) == TEST_ERR_TEST_FAIL)
+            return TEST_ERR_TEST_FAIL;
+        test_file_buf_ptr += bytes_read;
+        ++test_cnt;
+    }
+
+    utils_colored_fprintf(
+        stdout, 
+        ANSI_COLOR_BOLD_GREEN, 
+        "%d test%c passed successfully\n", 
+        test_cnt,
+        test_cnt == 1 ? ' ' : 's'
+    );
+
+    fclose(test_file);
+    free(test_file_buf);
+
+    return TEST_ERR_TEST_SUCCESS;
+}
+
+enum test_err_t one_test(struct test_data_t* test_data)
+{
+    utils_assert(test_data != NULL);
+
+    double root_a_test = 0.0, root_b_test = 0.0;
+    
+    solve_quadratic_equation(
+        test_data->coeff_a, 
+        test_data->coeff_b, 
+        test_data->coeff_c, 
+        &root_a_test, 
+        &root_b_test
+    );
+    
+    if(!utils_equal_with_precision_nan_inf(test_data->root_a, root_a_test) || 
+    !utils_equal_with_precision_nan_inf(test_data->root_b, root_b_test)) {
+        utils_colored_fprintf(
+            stderr, 
+            ANSI_COLOR_BOLD_RED, 
+            "Test failed. Expected: %lf, %lf, actual: %lf, %lf\n",
+            test_data->root_a,
+            test_data->root_b,
+            root_a_test,
+            root_b_test
+        );
+        return TEST_ERR_TEST_FAIL;
+    }
+    
+    return TEST_ERR_TEST_SUCCESS;
+}
+
+enum test_err_t read_one_test(char* filebuffer, struct test_data_t* test_data, int* bytes_read)
+{
+    utils_assert(filebuffer != NULL);
+    utils_assert(test_data != NULL);
+    utils_assert(bytes_read != NULL);
+    
+    int assigned = sscanf(filebuffer, "%lf %lf %lf %lf %lf%n", 
+        &test_data->coeff_a, 
+        &test_data->coeff_b, 
+        &test_data->coeff_c,
+        &test_data->root_b,
+        &test_data->root_a,
+        bytes_read
+    );
+
+    if(assigned != TEST_DATA_NUM_FIELDS)
+        return TEST_ERR_FILE_READ_ERR;
+
+    return TEST_ERR_FILE_READ_SUCCESS;
+}
+
+int utils_equal_with_precision_nan_inf(double a, double b)
+{
+    if(utils_isnan(a) && utils_isnan(b)) return 1;
+    else if(utils_isinf(a) && utils_isinf(b)) return 1;
+    else if(utils_equal_with_precision(a, b)) return 1;
+    return 0;
+}
+
+// FIXME почему везде используетсяя FILE* а не FILE
+// FIXME что происходит до main и после него
+// FIXME doxygen
+// FIXME README 
+
