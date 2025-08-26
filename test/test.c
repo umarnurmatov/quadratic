f8d5d7f745950a0fadaaec4812e34bac132ef0f7
umarnurmatov <umarkhonnur12@gmail.com>
2025-08-26 18:44:39 +0300

    #INIT

--- test/test.c
+++ test/test.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <threads.h>
 
 #include "quadratic.h"
 #include "ioutils.h"
 #include "colorutils.h"
 #include "floatutils.h"
+#include "logutils.h"
 
 static const size_t TEST_DATA_NUM_FIELDS = 5;
 /*
@@ -17,6 +19,10 @@ Single test is structured as followed:
 5. linear:    <c1> <c2> <c3> nan  nan (yeah, we don't solve linear eq)
 */
 static const char* TEST_INPUT_RELATIVE_FILEPATH = "test/input.txt";
+static const char* LOG_FILENAME = "log.txt";
+static const char* LOG_PATH = "log";
+
+static const int TEST_THREAD_NUM = 3;
 
 struct test_data_t
 {
@@ -36,39 +42,49 @@ enum test_err_t
     TEST_ERR_FILE_READ_SUCCESS
 };
 
-enum test_err_t all_test(const char* test_input_filename);
+struct test_thrd_data_t
+{
+    const char* input_filename;
+    enum test_err_t test_err;
+};
+
+int all_test(struct test_thrd_data_t* test_thrd_data);
 enum test_err_t one_test(struct test_data_t* test_data);
 enum test_err_t read_one_test(char* filebuffer, struct test_data_t* test_data, int* bytes_read);
 int utils_equal_with_precision_nan_inf(double a, double b);
 
 int main()
 {
-    enum test_err_t test_err = all_test(TEST_INPUT_RELATIVE_FILEPATH);
+    utils_init_log(LOG_FILENAME, LOG_PATH);
+    thrd_t threads[TEST_THREAD_NUM];
+    struct test_thrd_data_t test_data[TEST_THREAD_NUM];
 
-    switch(test_err) {
-        case TEST_ERR_TEST_FAIL:
-        case TEST_ERR_FILE_OPEN_ERR:
-        case TEST_ERR_FILE_READ_ERR:
-            return 1;
-            break;
-        case TEST_ERR_TEST_SUCCESS:
-        case TEST_ERR_FILE_READ_SUCCESS:
-            break;
-        default:
-            break;
+    for(int i = 0; i < TEST_THREAD_NUM; ++i) {
+        test_data[i].input_filename = TEST_INPUT_RELATIVE_FILEPATH;
+        thrd_create(&threads[i], (thrd_start_t)all_test, &test_data[i]);
     }
+
+    for(int i = 0; i < TEST_THREAD_NUM; ++i) {
+        int thrd_res;
+        thrd_join(threads[i], &thrd_res);
+    }
+    utils_end_log();
 
     return 0;
 }
 
-enum test_err_t all_test(const char* test_input_filename)
+int all_test(struct test_thrd_data_t* test_thrd_data)
 {
-    utils_assert(test_input_filename != NULL);
+    utils_assert(test_thrd_data->input_filename != NULL);
 
-    char* test_file_realpath = realpath(test_input_filename, NULL);
+    utils_log(LOG_LEVEL_INFO, "Running tests");
+
+    char* test_file_realpath = realpath(test_thrd_data->input_filename, NULL);
     FILE* test_file = open_file(test_file_realpath, "r");
-    if(test_file == NULL)
-        return TEST_ERR_FILE_OPEN_ERR;
+    if(test_file == NULL) {
+        test_thrd_data->test_err = TEST_ERR_FILE_OPEN_ERR;
+        return 0;
+    }
     free(test_file_realpath);
 
     char* test_file_buf = bufferize_file(test_file);
@@ -79,10 +95,14 @@ enum test_err_t all_test(const char* test_input_filename)
     int test_cnt = 0;
     struct test_data_t test_data;
     while(test_file_buf_ptr - test_file_buf < (long)test_file_size) {
-        if(read_one_test(test_file_buf_ptr, &test_data, &bytes_read) == TEST_ERR_FILE_READ_ERR)
-            return TEST_ERR_FILE_READ_ERR; 
-        if(one_test(&test_data) == TEST_ERR_TEST_FAIL)
-            return TEST_ERR_TEST_FAIL;
+        if(read_one_test(test_file_buf_ptr, &test_data, &bytes_read) == TEST_ERR_FILE_READ_ERR) {
+            test_thrd_data->test_err = TEST_ERR_FILE_READ_ERR;
+            return 0;
+        }
+        if(one_test(&test_data) == TEST_ERR_TEST_FAIL) {
+            test_thrd_data->test_err = TEST_ERR_TEST_FAIL;
+            return 0;
+        }
         test_file_buf_ptr += bytes_read;
         ++test_cnt;
     }
@@ -98,7 +118,7 @@ enum test_err_t all_test(const char* test_input_filename)
     fclose(test_file);
     free(test_file_buf);
 
-    return TEST_ERR_TEST_SUCCESS;
+    return 0;
 }
 
 enum test_err_t one_test(struct test_data_t* test_data)
@@ -142,8 +162,8 @@ enum test_err_t read_one_test(char* filebuffer, struct test_data_t* test_data, i
         &test_data->coeff_a, 
         &test_data->coeff_b, 
         &test_data->coeff_c,
-        &test_data->root_b,
         &test_data->root_a,
+        &test_data->root_b,
         bytes_read
     );
 
@@ -160,3 +180,6 @@ int utils_equal_with_precision_nan_inf(double a, double b)
     else if(utils_equal_with_precision(a, b)) return 1;
     return 0;
 }
+
+
+// FIXME обернуть стандартные мьютексы в макросы чтобы они разворачивались в 0 при однопотоке
